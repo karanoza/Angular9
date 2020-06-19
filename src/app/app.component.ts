4929c80058394d2715fdf0e1e4250a714fd0027a
Karan <karanoza94@gmail.com>
2020-06-19 15:54:36 +0530

    New code override

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-start',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  // template: `<mark>internal html</mark>`, // view : UI
-  styles: [
-    `
-      .my-class {
-        color: red;
-      }
-    `,
-  ],
+  selector: "app-start",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  // data and logic
-}
+  title = "Hello ..!";
 
-// static web App  :  const data
-// Dynamic Web App :  data with var
-// { }    : res for obj, block
-// {{ }}  : interpolation : [ data binding ] to print var
-//   '' -> ``
+  names = [
+    "Alex",
+    "Ali",
+    "Sai",
+    "Alex",
+    "Ali",
+    "Sai",
+    "Alex",
+    "Ali",
+    "Sai",
+    "Alex",
+    "Ali",
+    "Sai"
+  ];
+}
--- src/app/directives/simple-style.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SimpleStyleDirective } from './simple-style.directive';
-
-describe('SimpleStyleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SimpleStyleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
